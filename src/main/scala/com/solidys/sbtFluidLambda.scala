41f9db18957caa73b661ea364a0bdccedce55c2c
Joseph Hajduk <joseph@solidys.com>
2016-08-04 21:37:25 -0600

    sbt is strange

--- src/main/scala/com/solidys/sbtFluidLambda.scala
+++ src/main/scala/com/solidys/sbtFluidLambda.scala
@@ -35,62 +35,61 @@ object sbtFluidLambda extends Plugin
   val lambdaAwsRegion = SettingKey[String]("AWS region to deploy to")
   val lambdaHandler = SettingKey[String]("handler object")
 
-  prepareNodePayload in Compile <<= (fullOptJS in Compile, packageJSDependencies in Compile, target) map {
-    (jsFile, depsFile, tf) =>
-
-      val zipFile = tf / "lambda.zip"
-      val inputs: Seq[(File, String)] = Seq((jsFile.data, "index.js"))
-
-      IO.zip(inputs, zipFile)
-
-      zipFile
-  }
-
-  prepareJVMPayload in Compile <<= (assembly in Compile) map {
-    (jar) => {
-      jar
-    }
-  }
-
-  createLambda in Compile <<= (
-    name,
-    organization,
-    version,
-    prepareJVMPayload in Compile,
-    prepareNodePayload in Compile,
-    lambdaDisableJVM,
-    lambdaDisableNode,
-    lambdaExecutionRole,
-    lambdaAwsRegion,
-    lambdaHandler) map {
-    (n,o,v,jvmPayload,nodePayload,disableJVM,disableNode,execRole,region,handler) => {
-      val client = new AWSLambdaClient()
-      client.setRegion(Region.getRegion(Regions.fromName(region)))
-
-      val jvmFS = new FileInputStream(jvmPayload)
-      val jvmBuffer = ByteBuffer.wrap(Stream.continually(jvmFS.read).takeWhile(_ != -1).map(_.toByte).toArray)
-
-      val nodeFS = new FileInputStream(nodePayload)
-      val nodeBuffer = ByteBuffer.wrap(Stream.continually(nodeFS.read).takeWhile(_ != -1).map(_.toByte).toArray)
-
-      val namePattern = "(arn:aws:lambda:)?([a-z]{2}-[a-z]+-\\d{1}:)?(\\d{12}:)?(function:)?([a-zA-Z0-9-_]+)(:(\\$LATEST|[a-zA-Z0-9-_]+))?"
-
-      val nameBase = o+"_"+n+"_" + handler + "_" + v
-      val nameJVM = (nameBase + "_JVM").replace(".","-")
-      val nameNode = (nameBase + "_NODE").replace(".","-")
-
-      if (!nameJVM.matches(namePattern) || nameJVM.length > 256)
-        throw new Exception(nameJVM+" does not match the required regex: "+namePattern+" or is longer than 256 characters")
-      else if (!nameNode.matches(namePattern) || nameNode.length > 256)
-        throw new Exception(nameJVM+" does not match the required regex: "+namePattern+" or is longer than 256 characters")
-      else {
-        println(nameJVM)
-        println(nameNode)
-      }
-    }
-  }
-
   val defaultSettings = Seq(
+    prepareNodePayload in Compile <<= (fullOptJS in Compile, packageJSDependencies in Compile, target) map {
+      (jsFile, depsFile, tf) =>
+
+        val zipFile = tf / "lambda.zip"
+        val inputs: Seq[(File, String)] = Seq((jsFile.data, "index.js"))
+
+        IO.zip(inputs, zipFile)
+
+        zipFile
+    },
+
+    prepareJVMPayload in Compile <<= (assembly in Compile) map {
+      (jar) => {
+        jar
+      }
+    },
+
+    createLambda in Compile <<= (
+      name,
+      organization,
+      version,
+      prepareJVMPayload in Compile,
+      prepareNodePayload in Compile,
+      lambdaDisableJVM,
+      lambdaDisableNode,
+      lambdaExecutionRole,
+      lambdaAwsRegion,
+      lambdaHandler) map {
+      (n,o,v,jvmPayload,nodePayload,disableJVM,disableNode,execRole,region,handler) => {
+        val client = new AWSLambdaClient()
+        client.setRegion(Region.getRegion(Regions.fromName(region)))
+
+        val jvmFS = new FileInputStream(jvmPayload)
+        val jvmBuffer = ByteBuffer.wrap(Stream.continually(jvmFS.read).takeWhile(_ != -1).map(_.toByte).toArray)
+
+        val nodeFS = new FileInputStream(nodePayload)
+        val nodeBuffer = ByteBuffer.wrap(Stream.continually(nodeFS.read).takeWhile(_ != -1).map(_.toByte).toArray)
+
+        val namePattern = "(arn:aws:lambda:)?([a-z]{2}-[a-z]+-\\d{1}:)?(\\d{12}:)?(function:)?([a-zA-Z0-9-_]+)(:(\\$LATEST|[a-zA-Z0-9-_]+))?"
+
+        val nameBase = o+"_"+n+"_" + handler + "_" + v
+        val nameJVM = (nameBase + "_JVM").replace(".","-")
+        val nameNode = (nameBase + "_NODE").replace(".","-")
+
+        if (!nameJVM.matches(namePattern) || nameJVM.length > 256)
+          throw new Exception(nameJVM+" does not match the required regex: "+namePattern+" or is longer than 256 characters")
+        else if (!nameNode.matches(namePattern) || nameNode.length > 256)
+          throw new Exception(nameJVM+" does not match the required regex: "+namePattern+" or is longer than 256 characters")
+        else {
+          println(nameJVM)
+          println(nameNode)
+        }
+      }
+    },
     lambdaDisableJVM := false,
     lambdaDisableNode := false
   )

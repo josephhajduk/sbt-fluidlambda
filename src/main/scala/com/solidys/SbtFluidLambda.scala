fb4a2aa9ac6f9d1edcec19554f854752c9c0ea51
Joseph Hajduk <joseph@solidys.com>
2016-08-11 17:10:07 -0600

    added apache 2.0 license

--- src/main/scala/com/solidys/SbtFluidLambda.scala
+++ src/main/scala/com/solidys/SbtFluidLambda.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Solidys Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.solidys
 
 import java.io.FileInputStream
@@ -75,7 +91,7 @@ object SbtFluidLambda extends AutoPlugin {
 
       preparePayload := dynPayload.value,
 
-      prepareJVMPayload in Compile <<= (assembly) map {
+      prepareJVMPayload in Compile <<= assembly map {
         (asm) =>
           asm
       },
@@ -109,7 +125,7 @@ object SbtFluidLambda extends AutoPlugin {
             .withPayload(testEvent)
           )
 
-          println("result: "+ (new String(ir.getPayload.array())))
+          println("result: "+ new String(ir.getPayload.array()))
         }
       },
 
